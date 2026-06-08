3eb73ed7bef3141ed505396638766c17921f394e
koooogi <astronaut.kogi@gmail.com>
2026-06-08 21:12:23 +0300

    structure

---
+++ src/main/java/com/example/planner/Planner.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.planner;
+
+/**
+ *
+ * @author kogi <astronaut.kogi@gmail.com>
+ */
+public class Planner {
+
+    public static void main(String[] args) {
+    }
+}
